1d7305c408c290630a9e13f21c22f70609a96caa
alpaca <dragonpepper95@gmail.com>
2023-06-21 16:14:15 +0900

    first

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import  crab from './nooooo.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={crab}/>
+    </div>
+  );
+}
+
+export default App;
